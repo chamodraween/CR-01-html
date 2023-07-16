c787736ceb9c62dfd6afcfd1f87a559a2ca5e102
Chamod Raween <wimalasinghe355@gmail.com>
2023-07-16 10:03:10 +0530

    java script Global variable Local variable

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+console.log("Hello Word")
+
+// Variable type 3
+//     1. var
+//     2. let
+//     3. const
+
+// global variable and local variable
+
+var name = "Chamod"; // Global variable
+Check()
+
+function Check(){
+    var name = "Raween" // Local variable
+    document.write(name)
+}
+
+document.write(name)
